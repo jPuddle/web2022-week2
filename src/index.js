4e9fec2007df6aee0457c62eed3a47d456c1f323
jPuddle <mikkojjutila@gmail.com>
2022-09-15 16:06:03 +0300

    Add images

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function initializeCode() {
   const emailInput = document.getElementById("input-email");
   const addressInput = document.getElementById("input-address");
   const adminInput = document.getElementById("input-admin");
+  const imageInput = document.getElementById("input-image");
 
   const addUserButton = document.getElementById("submit-data");
   addUserButton.addEventListener("click", () =>
@@ -22,7 +23,8 @@ function initializeCode() {
       usernameInput.value,
       emailInput.value,
       addressInput.value,
-      adminInput.checked
+      adminInput.checked,
+      imageInput.files[0] || null
     )
   );
 
@@ -33,17 +35,18 @@ function initializeCode() {
   });
 }
 
-function addUser(username, email, address, admin) {
+function addUser(username, email, address, admin, image) {
   let index = users.findIndex((user) => user.username === username);
   if (index === -1) {
     users.push({
       username: username,
       email: email,
       address: address,
-      admin: admin
+      admin: admin,
+      image: image
     });
   } else {
-    users[index] = { username, email, address, admin };
+    users[index] = { username, email, address, admin, image };
   }
   drawUsersTable();
 }
@@ -51,8 +54,8 @@ function addUser(username, email, address, admin) {
 function drawUsersTable() {
   const usersTable = document.getElementById("users-table");
   usersTable.innerHTML =
-    "<tr><td>Username</td><td>Email</td><td>Address</td><td>Admin</td></tr>";
-  users.forEach(({ username, email, address, admin }) => {
+    "<tr><td>Username</td><td>Email</td><td>Address</td><td>Admin</td><td>Image</td></tr>";
+  users.forEach(({ username, email, address, admin, image }) => {
     let newTr = document.createElement("tr");
 
     [username, email, address].forEach((data) => {
@@ -64,6 +67,16 @@ function drawUsersTable() {
     let adminTd = document.createElement("td");
     adminTd.innerText = admin ? "X" : "-";
     newTr.appendChild(adminTd);
+
+    let imageTd = document.createElement("td");
+    if (image) {
+      let imageEl = document.createElement("img");
+      imageEl.src = URL.createObjectURL(image);
+      imageEl.height = 64;
+      imageEl.width = 64;
+      imageTd.appendChild(imageEl);
+    }
+    newTr.appendChild(imageTd);
 
     usersTable.appendChild(newTr);
   });
